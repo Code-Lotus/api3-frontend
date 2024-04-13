4708a5e150736069e2d751a12657f273faeb72e6
joao-eduardo17 <125677846+joao-eduardo17@users.noreply.github.com>
2024-04-13 07:43:42 -0300

    atualizações do front

--- src/components/bloco/index.tsx
+++ src/components/bloco/index.tsx
@@ -8,36 +8,40 @@ export default function Bloco({v1,v2,v3,v4}: {v1: Array<number>, v2: Array<numbe
     return (
         <div className={Style.dashboard}>
             <div className={Style.dashboardBlocos}>
-                <h3>Produto Novo</h3>
-                <div className={Style.dashboardBlocosProduto}>
-                    <section>
-                        <h4>Cliente novo</h4>
-                        <div className={Style.grafico}>
-                            <Linha categoria={["Janeiro", "Fevereiro", "Março", "Abril", "Maio"]} nome="produto" valor={[1,2,3,4,10]} />
-                        </div>
-                    </section>
-                    <section>
-                        <h4>Cliente antigo</h4>
-                        <div className={Style.grafico}>
-                            <Linha categoria={["Janeiro", "Fevereiro", "Março", "Abril", "Maio"]} nome="produto" valor={[1,2,3,4,5]} />
-                        </div>
-                    </section>
-                </div>
-                <h3>Produto antigo</h3>
-                <div className={Style.dashboardBlocosProduto}>
-                    <section>
-                        <h4>Cliente novo</h4>
-                        <div className={Style.grafico}>
-                            <Linha categoria={["Janeiro", "Fevereiro", "Março", "Abril", "Maio"]} nome="produto" valor={[1,2,3,4,5]}/>
-                        </div>
-                    </section>
-                    <section>
-                        <h4>Cliente antigo</h4>
-                        <div className={Style.grafico}>
-                            <Linha categoria={["Janeiro", "Fevereiro", "Março", "Abril", "Maio"]} nome="produto" valor={[1,2,3,5,7 ]}/>
-                        </div>
-                    </section>
-                </div>
+                <div className={Style.dashboardGraficos}>
+                    <h3>Produto Novo</h3>
+                    <div className={Style.dashboardBlocosProduto}>
+                        <section>
+                            <h4>Cliente novo</h4>
+                            <div className={Style.grafico}>
+                                <Linha categoria={["Janeiro", "Fevereiro", "Março", "Abril", "Maio"]} nome="produto" valor={[1,2,3,4,10]} />
+                            </div>
+                        </section>
+                        <section>
+                            <h4>Cliente antigo</h4>
+                            <div className={Style.grafico}>
+                                <Linha categoria={["Janeiro", "Fevereiro", "Março", "Abril", "Maio"]} nome="produto" valor={[1,2,3,4,5]} />
+                            </div>
+                        </section>
+                    </div>
+                </div> 
+                <div className={Style.dashboardGraficos}>
+                    <h3>Produto Antigo</h3>
+                    <div className={Style.dashboardBlocosProduto}>
+                        <section>
+                            <h4>Cliente novo</h4>
+                            <div className={Style.grafico}>
+                                <Linha categoria={["Janeiro", "Fevereiro", "Março", "Abril", "Maio"]} nome="produto" valor={[1,2,3,4,5]}/>
+                            </div>
+                        </section>
+                        <section>
+                            <h4>Cliente antigo</h4>
+                            <div className={Style.grafico}>
+                                <Linha categoria={["Janeiro", "Fevereiro", "Março", "Abril", "Maio"]} nome="produto" valor={[1,2,3,5,7 ]}/>
+                            </div>
+                        </section>
+                    </div>
+                </div>               
             </div>
             <div className={Style.graficoPizza}>
                 <h3 className="tituloGraficoPizza">Geral</h3>
