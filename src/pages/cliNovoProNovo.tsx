429bfeb88f4dca1ea9a8e4015e27f81561d464e8
joao-eduardo17 <125677846+joao-eduardo17@users.noreply.github.com>
2024-04-18 04:15:38 -0300

    tentativa de utilizar router

---
+++ src/pages/cliNovoProNovo.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function CliNovoProNovo(){
+    return(
+        <>
+            <p>Hello World!</p>
+            <Link to="../app">Home</Link>
+        </>
+    );
+}
