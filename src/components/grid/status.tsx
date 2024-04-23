ac432b8b393e6dc6c79a9270508abeabfc2a38e3
Sandro-Pimentel <125677890+Sandro-Pimentel@users.noreply.github.com>
2024-04-23 11:01:13 -0300

    Componentizando card
    Componentizando card para o componente grid.

--- src/components/grid/status.tsx
+++ src/components/grid/status.tsx
@@ -1,3 +1,4 @@
+import Card from "../card";
 import "./status.css";
 
 export default function Status() {
@@ -5,11 +6,7 @@ export default function Status() {
             <ul className="box">
                     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link>
                 <li>
-                    <i className='bx bxs-calendar-plus'></i>
-                    <span>
-                     <h3>66</h3>
-                    <p>Vendas novas</p>
-                    </span>
+                    <Card classeCss="bx bxs-calendar-plus" quantidade="66" titulo="Vendas novas"/>
                 </li>
 
                 <li>
