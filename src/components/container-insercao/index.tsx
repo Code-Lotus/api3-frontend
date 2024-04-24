b94accaebda154698310184f2435cddd09ceb737
Sandro-Pimentel <125677890+Sandro-Pimentel@users.noreply.github.com>
2024-04-24 09:44:00 -0300

    Criando o componente container inserção
    Criando o componente container inserção e ajustes no vendas controller.

---
+++ src/components/container-insercao/index.tsx
@@ -0,0 +1,23 @@
+import { Component } from "react";
+import Style from "./containerInsercao.module.scss";
+//import './style.css';
+
+export default class ContainerInsercao extends Component {
+    render() {
+        return (
+            <div className={Style.dateTable}>
+				<div className={Style.order}>
+					<div className={Style.head}>
+						<h3>{}</h3>
+						{/* <i className='bx bx-search' ></i>
+						<i className='bx bx-filter' ></i> */}
+					</div>
+					<div>
+						<p> Insira o arquivo excel para armazenar: </p>
+						<input type="file"/>
+					</div>
+				</div>
+			</div>
+        )
+    }
+}
--- src/pages/cliNovoProNovo.tsx
+++ src/pages/cliNovoProNovo.tsx
@@ -1,12 +1,12 @@
 import { Link }    from "react-router-dom";
 import Navbar from "../components/navbar";
 import Sidebar from "../components/sidebar";
+import ContainerInsercao from "../components/container-insercao";
 
 export default function CliNovoProNovo(){
     return(
         <>
-            <Navbar />
-            <Sidebar />
+            <ContainerInsercao />
         </>
     );
 }
