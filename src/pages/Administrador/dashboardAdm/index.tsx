70caf5eb1be61ff07e57a0ec1c8ca4eeaf44dd4c
Kalil Pereira <kalil.alves.2019@gmail.com>
2024-04-25 08:47:59 -0300

    Homepage do admnistrador 4/4
    Faltando apenas o componente de lista (faz aí vitor)

--- src/pages/Administrador/dashboardAdm/index.tsx
+++ src/pages/Administrador/dashboardAdm/index.tsx
@@ -1,11 +1,21 @@
-// import Navbar from "../components/navbar"
-// import Sidebar from "../components/sidebar"
+import Navbar from "../../../components/navbar"
+import Sidebar from "../../../components/sidebar"
 import Grid from "../../../components/grid";
+import Style from "../dashboardAdm/dashboardAdm.module.scss"
+import ContainerGrafico from "../../../components/container-adm";
 
 export default function DashboardAdm(){
     return(
-        <div>
-            <Grid />
+        <>
+        <Navbar />
+        <Sidebar />
+        <div className={Style.cards}>
+          <div className={Style.topTitle}>
+            <h1>Bem-vindo, administrador</h1>
+          </div>
+          <Grid />
+          <ContainerGrafico />
         </div>
-    );
+      </>
+      );
 }
