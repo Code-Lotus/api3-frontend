1872a4df1319c9a44b2635f19bbf12386b302ffa
Kalil Pereira <kalil.alves.2019@gmail.com>
2024-04-25 11:54:49 -0300

    Correção na homepage de adm

--- src/pages/Administrador/dashboardAdm/index.tsx
+++ src/pages/Administrador/dashboardAdm/index.tsx
@@ -1,21 +1,25 @@
-import Navbar from "../../../components/navbar"
-import Sidebar from "../../../components/sidebar"
+import Navbar from "../../../components/navbar";
+import Sidebar from "../../../components/sidebar";
 import Grid from "../../../components/grid";
-import Style from "../dashboardAdm/dashboardAdm.module.scss"
-import ContainerGrafico from "../../../components/container-adm";
+import Style from "../dashboardAdm/dashboardAdm.module.scss";
+import Historico from "../../../components/historico";
+import Pizza from "../../../components/graficos/pie";
 
 export default function DashboardAdm(){
     return(
-        <>
-        <Navbar />
-        <Sidebar />
-        <div className={Style.cards}>
-          <div className={Style.topTitle}>
-            <h1>Bem-vindo, administrador</h1>
-          </div>
-          <Grid />
-          <ContainerGrafico />
+      <>
+      <Navbar />
+      <Sidebar />
+      <div className={Style.cards}>
+        <div className={Style.topTitle}>
+          <h1>Bem-vindo, administrador</h1>
         </div>
-      </>
+        <Grid />
+        <section className={Style.grafico}>
+          <Pizza valores={[25,25,25,25]} legenda={['Cliente Novo / Produto Novo', 'Cliente Antigo / Produto Novo', 'Cliente Antigo / Produto Antigo', 'Cliente Novo / Produto Antigo']} />
+          <Historico/>
+        </section>
+      </div>
+    </>
       );
 }
