138ed54323cedf8e4c35355487c2fb9102528863
ThiagoCanedo <thiago.freitas17@outlook.com>
2024-05-02 18:21:44 -0300

    inicio ajuste tabela pag adm

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import CliNovoProAntigo from "./pages/Vendedor/cliNovoProAntigo/cliNovoProAntigo";
 import DashboardAdm from "./pages/Administrador/dashboardAdm";
+import CliNovoProAntigo from "./pages/Vendedor/cliNovoProAntigo/cliNovoProAntigo";
 import CliAntigoProNovo from "./pages/Vendedor/cliAntigoProNovo/cliAntigoProNovo";
 import CliAntigoProAntigo from "./pages/Vendedor/cliAntigoProAntigo/cliAntigoProAntigo";
 import DashboardVendedor from "./pages/Vendedor/dashboardVendedor/dashboardVendedor";
@@ -14,14 +14,17 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/">
-          <Route path="/" element={<DashboardVendedor/>} />
+          <Route path="/dashboardAdm" element={<DashboardAdm />}/>
+          {/* <Route path="/" element={<DashboardVendedor/>} />
           <Route path="/cliNovoProNovo" element={<CliNovoProNovo />} />
           <Route path="/cliNovoProAntigo" element={<CliNovoProAntigo />} />
           <Route path="/cliAntigoProNovo" element={<CliAntigoProNovo />} />
-          <Route path="/cliAntigoProAntigo" element={<CliAntigoProAntigo />} />
-          <Route path="/dashboardAdm" element={<DashboardAdm />}/>
-          <Route path="/comissaoAdm" element={<ComissaoAdm />}/>
-          <Route path="/insercaoExcel" element={<InsercaoExcel />}/>
+          <Route path="/cliAntigoProAntigo" element={<CliAntigoProAntigo />} /> */} 
+          
+          
+
+          {/* <Route path="/comissaoAdm" element={<ComissaoAdm />}/>
+          <Route path="/insercaoExcel" element={<InsercaoExcel />}/> */}
         </Route>
       </Routes>
     </BrowserRouter>
