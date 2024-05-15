1df7ba7f048cd7c6966ebcb99ca6f925d1770860
Kalil Pereira <kalil.alves.2019@gmail.com>
2024-05-15 12:18:50 -0300

    frontend da interface de cadastro

--- src/pages/Usuário/cadastro/cadastro.tsx
+++ src/pages/Usuário/cadastro/cadastro.tsx
@@ -7,10 +7,11 @@ export default function Cadastro() {
     const dadosController = new DadosController();
 
     const ajustarCpf = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const inputCPF= event.target.value;
+        const inputCPF = event.target.value;
         const maskCPF = dadosController.mascaraCPF(inputCPF);
         setCpf(maskCPF);
     }
+
     return (
         <>
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link>
@@ -19,24 +20,32 @@ export default function Cadastro() {
             <h1>Cadastro</h1>
             </div>
                     <div>
-                    <i className='bx bx-user-circle'></i> &nbsp;
                     <input type='text' placeholder="Nome"></input>
+                    <i className='bx bx-user-circle'></i> &nbsp;
                     </div>
 
                     <div>
-                    <i className='bx bx-id-card'></i> &nbsp;
                     <input type='text' id='cpf' value={cpf} onChange={ajustarCpf}
                     placeholder="CPF"/>
+                    <i className='bx bx-id-card'></i> &nbsp;
                     </div>
 
                     <div>
-                    <i className='bx bx-envelope' ></i> &nbsp;
                     <input type='email' placeholder="E-mail"></input>
+                    <i className='bx bx-envelope' ></i> &nbsp;
                     </div>
 
-                    <div>
-                    <i className='bx bx-lock-alt'></i> &nbsp;
-                    <input type='password' placeholder="Senha"></input>
+                    <div className="password-container">
+                        <input type='password' placeholder="Senha"></input>
+                        <i className='bx bx-lock-alt'></i>
+                    </div>
+
+            <div>
+                <a href="#"><button><h3>Registrar</h3></button></a> 
+            </div>
+
+                    <div className="login">
+                        Já é um usuário?<a href="#"> Faça login aqui</a>
                     </div>
         </div>
         </>
