f3d6ce50410d38ac1632009d75d8aa63ef58d3fb
Kalil Pereira <kalil.alves.2019@gmail.com>
2024-05-16 12:17:40 -0300

    pequenos ajustes

--- src/pages/Usuário/cadastro/cadastro.tsx
+++ src/pages/Usuário/cadastro/cadastro.tsx
@@ -1,5 +1,3 @@
-// Cadastro.tsx
-
 import React, { useState } from "react";
 import Style from "../cadastro/cadastro.module.scss";
 import DadosController from "../../../scripts/controllers/dados-controller";
@@ -24,38 +22,39 @@ export default function Cadastro() {
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link>
         <div className={Style.register}>
             <div className={Style.topTitle}>
-                <h1>Cadastro</h1>
+            <h1>Cadastro</h1>
             </div>
-            <div>
-                <input type='text' placeholder="Nome"></input>
-                <i className='bx bx-user-circle'></i> &nbsp;
-            </div>
+                    <div>
+                    <input type='text' placeholder="Nome"></input>
+                    <i className='bx bx-user-circle'></i> &nbsp;
+                    </div>
 
-            <div>
-                <input type='text' id='cpf' value={cpf} onChange={ajustarCpf} placeholder="CPF"/>
-                <i className='bx bx-id-card'></i> &nbsp;
-            </div>
+                    <div>
+                    <input type='text' id='cpf' value={cpf} onChange={ajustarCpf}
+                    placeholder="CPF"/>
+                    <i className='bx bx-id-card'></i> &nbsp;
+                    </div>
 
-            <div>
-                <input type='email' placeholder="E-mail"></input>
-                <i className='bx bx-envelope' ></i> &nbsp;
-            </div>
+                    <div>
+                    <input type='email' placeholder="E-mail"></input>
+                    <i className='bx bx-envelope' ></i> &nbsp;
+                    </div>
 
-            <div>
-                <input type={showPassword ? 'text' : 'password'} placeholder="Senha"></input>
-                <i className='bx bx-lock-alt'></i>
-                <div className="showPass">
-                    <span className={showPassword ? 'bx bxs-show' : 'bx bxs-low-vision'} onClick={togglePasswordVisibility}></span>
-                </div>
-            </div>
+                    <div>
+                        <input type={showPassword ? 'text' : 'password'} placeholder="Senha"></input>
+                        <i className='bx bx-lock-alt'></i>
+                        <div className="showPass">
+                        <span className={showPassword ? 'bx bxs-show' : 'bx bxs-low-vision'} onClick={togglePasswordVisibility}></span>
+                        </div>
+                    </div>
 
             <div>
                 <a href="#"><button><h3>Registrar</h3></button></a> 
             </div>
 
-            <div className="login">
-                Já é um usuário?<a href="#"><b> Faça login aqui</b></a>
-            </div>
+                    <div className="login">
+                        Já é um usuário?<a href="#"><b> Faça login aqui</b></a>
+                    </div>
         </div>
         </>
     )
