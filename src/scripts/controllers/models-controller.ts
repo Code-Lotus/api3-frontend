4b3081af7cfc98c44342978e71c473be14fd3054
Sandro-Pimentel <125677890+Sandro-Pimentel@users.noreply.github.com>
2024-05-27 09:23:19 -0300

    Update models-controller.ts

--- src/scripts/controllers/models-controller.ts
+++ src/scripts/controllers/models-controller.ts
@@ -8,38 +8,72 @@ import Vendedor from "../models/vendedor";
 export default class ModelsController {
     public async chamaVendas() {
         const vendas = await api.get("/vendas");
+        console.log(vendas.data)
         return vendas.data
     }
     
-    private async buscaUsuario(id: number) {
-        const response = await api.patch("/usuario", {
-            usuario_id: id
-        });
+    private async chamaUsuarios() {
+        const response = await api.get("/usuarios");
         console.log(response.data)
         return response.data
     }
 
-    private async buscaCliente(id: number) {
-        const response = await api.patch("/cliente", {
-            cliente_id: id
-        });
+    private async chamaClientes() {
+        const response = await api.get("/clientes");
         console.log(response.data)
         return response.data
     }
 
-    private async buscaProduto(id: number) {
-        const response = await api.patch("/produto", {
-            produto_id: id
-        });
+    private async chamaProdutos() {
+        const response = await api.get("/produtos");
         console.log(response.data)
         return response.data
+    }
+
+    private buscaUsuario(usuarios: any, id: number) {
+        let retorno: any;
+        usuarios.forEach((usuario: { usuario_id: number; }) => {
+            if(usuario.usuario_id == id){
+                console.log("SODA!!!!")
+                retorno = usuario
+            }
+        })
+        console.log("OBAMNA")
+        return retorno
+    }
+
+    private buscaCliente(usuarios: any, id: number) {
+        let retorno: any;
+        usuarios.forEach((usuario: { cliente_id: number; }) => {
+            if(usuario.cliente_id == id){
+                console.log("SODA!!!!")
+                retorno = usuario
+            }
+        })
+        console.log("OBAMNA")
+        return retorno
+    }
+
+    private buscaProduto(usuarios: any, id: number) {
+        let retorno: any;
+        usuarios.forEach((usuario: { produto_id: number}) => {
+            if(usuario.produto_id == id){
+                console.log("SODA!!!!")
+                retorno = usuario
+            }
+        })
+        console.log("OBAMNA")
+        return retorno
     }
 
     public async converteVenda(venda: any) {
-        const usuario = await this.buscaUsuario(venda.usuarioId);
-        const cliente = await this.buscaCliente(venda.clienteId);
-        const produto = await this.buscaProduto(venda.produtoId);
-
+        const usuarios = this.chamaUsuarios();
+        const usuario = this.buscaUsuario(usuarios , venda.usuario_id)
+        const clientes = this.chamaClientes();
+        const cliente = this.buscaCliente(clientes , venda.cliente_id)
+        const produtos = this.chamaProdutos();
+        const produto = this.buscaProduto(produtos , venda.produto_id)
+        
         const newUsuario = new Vendedor(usuario.usuario_cpf, usuario.usuario_nome);
         const newCliente = new Cliente(cliente.cliente_cpfcnpj, cliente.cliente_id, cliente.cliente_segmento, cliente.cliente_data);
         const newProduto = new Produto(produto.produto_id, produto.produto_nome, produto.produto_data);
