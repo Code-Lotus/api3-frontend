b9ed69a53fb75397f9ebe8a51d53cbbca4f988b3
Kalil Pereira <kalil.alves.2019@gmail.com>
2024-06-05 11:31:27 -0300

    upgrade das tabelas e começo da função de deletar

--- src/pages/Tabelas/clientes/index.tsx
+++ src/pages/Tabelas/clientes/index.tsx
@@ -29,28 +29,33 @@ export default function Clientes() {
         <Navbar/>
         <SidebarAdm/>
         <div className={Style.usuariosTableContainer}>
-            <section className={Style.usuariosTable}>
-                <table className={Style.tabela}>
-                    <thead>
-                        <tr>
-                            {titulos.map(titulo => (
-                                <th>{titulo}</th>
-                            ))}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {clientes.map(cliente => (
-                            <tr>
-                                <td>{cliente.cliente_id}</td>
-                                <td>{cliente.cliente_nome}</td>
-                                <td>{cliente.cliente_cpfcnpj}</td>
-                                <td>{cliente.cliente_segmento}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </section>
-        </div>
+        <section className={Style.usuariosTable}>
+        <table className={Style.tabela}>
+            <thead>
+                <tr>
+                    {titulos.map(titulo => (
+                        <th key={titulo}>{titulo}</th>
+                    ))}
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {clientes.map(cliente => (
+                    <tr key={cliente.cliente_id}>
+                        <td>{cliente.cliente_id}</td>
+                        <td>{cliente.cliente_nome}</td>
+                        <td>{cliente.cliente_cpfcnpj}</td>
+                        <td>{cliente.cliente_segmento}</td>
+                        <td>
+                            <button className={Style.btn}>Editar</button>
+                            <button className={Style.btn}>Apagar</button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </section>
+</div>
         </>
     )
 }
