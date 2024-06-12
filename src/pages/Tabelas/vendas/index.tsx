eead50b3cccc2043a3f0759305d2a05e0c3f1b7b
joao-eduardo17 <125677846+joao-eduardo17@users.noreply.github.com>
2024-06-12 10:47:54 -0300

    tabela Vendas como função e deletar venda

--- src/pages/Tabelas/vendas/index.tsx
+++ src/pages/Tabelas/vendas/index.tsx
@@ -1,28 +1,10 @@
-import { Component, ReactNode, useEffect, useState } from "react";
+import Style from "../usuarios/usuarios.module.scss"
+import ModelsController from "../../../scripts/controllers/models-controller";
+import DadosController from "../../../scripts/controllers/dados-controller";
+import { useEffect, useState } from "react";
+import { api } from "../../../services/api";
 import Navbar from "../../../components/navbar";
 import SidebarAdm from "../../../components/sidebar/adm";
-import Style from "../usuarios/usuarios.module.scss"
-import { api } from "../../../services/api";
-import Cliente from "../../../scripts/models/cliente";
-import Produto from "../../../scripts/models/produto";
-import Vendedor from "../../../scripts/models/vendedor";
-import ModelsController from "../../../scripts/controllers/models-controller";
-import PlanilhaVendas from "../../../scripts/models/planilhaVendas";
-import DadosController from "../../../scripts/controllers/dados-controller";
-// import Cliente from "../../../scripts/models/cliente";
-
-// interface VendasProps {
-//     venda_id: number,
-//     venda_data: Date,
-//     venda_forma_pagamento: string,
-//     cliente_id: number,
-//     produto_id: number,
-//     usuario_id: number
-// }
-
-type State = {
-    vendas: Array<VendasProps>
-}
 
 interface VendasProps {
     venda_id: number,
@@ -53,91 +35,73 @@ interface UsuarioProps {
 const modelsController = new ModelsController()
 const dadosController = new DadosController()
 
-
-export default class Vendas extends Component<{}, State> {
-    constructor(props: any){
-        super(props);
-        this.state = {vendas: []}
-    }
+export default function Vendas() {
+    const titulos = ["ID", "DATA", "VENDEDOR", "PRODUTO", "PRODUTO ID", "CLIENTE", "CPF/CNPJ", "SEGMENTO", "VALOR", "FORMA DE PAGAMENTO", "AÇÕES"]
+    const [vendas, setVendas] = useState<VendasProps[]>([]);
     
+    useEffect(() => {
+        carregaVendas();
+    }, [])
 
-    public titulos = ["ID", "DATA", "VENDEDOR", "PRODUTO", "PRODUTO ID", "CLIENTE", "CPF/CNPJ", "SEGMENTO", "VALOR", "FORMA DE PAGAMENTO", "AÇÕES"]
-    
-    async componentDidMount() {
-        await this.carregaVendas(this.state.vendas)
-    }
-
-    async componentDidUpdate() {
-        // await this.componentDidMount()
-    }
-    
-    public async carregaVendas(vendas: any) {
+    async function carregaVendas() {
         const response = await api.get("/vendas?page=1&per_page=5")
-        // let lista: any = []
+        let lista: any = []
         for(const venda of response.data) {
             const listaNova = await modelsController.converteVenda(venda)
-            vendas.push(listaNova)
-            // console.log(listaNova)
+            lista.push(listaNova)
+            // console.log(vendas1)
             // console.log(vendas[0]._vendedor)
         }
-        this.setState(vendas)   
+        console.log(vendas)
+        setVendas(lista)
     }
 
-    public async deletaVendas(vendaId: number) {
+    async function deletaVendas(vendaId: number) {
         console.log(vendaId)
         const response = await api.delete("/venda", {
             data: {
                 venda_id: vendaId
             }
-        });
-        // this.carregaVendas(this.state.vendas)
-        this.componentDidMount()
-
-        // setProdutos(produtos.filter(produto => produto.produto_id !== produtoId));
+        })
+        carregaVendas()
     }
 
-    
-
-    render(){
-        const { vendas } = this.state;
-        return (
-            <>
-                <Navbar />
-                <SidebarAdm />
-                <div className={Style.usuariosTableContainer}>
-                    <section className={Style.usuariosTable}>
-                        <table className={Style.tabela}>
-                            <thead>
-                                <tr>
-                                    {this.titulos.map((titulo, index) => (
-                                        <th key={index}>{titulo}</th>
-                                    ))}
+    return(
+        <>
+            <Navbar />
+            <SidebarAdm />
+            <div className={Style.usuariosTableContainer}>
+                <section className={Style.usuariosTable}>
+                    <table className={Style.tabela}>
+                        <thead>
+                            <tr>
+                                {titulos.map((titulo, index) => (
+                                    <th key={index}>{titulo}</th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* <td>{this.state.vendas[0].usuario.nome}</td> */}
+                            {vendas.map((venda: any,index: any) => index%2==0? (
+                                <tr key={index}>
+                                    <td>{venda.id}</td>
+                                    <td>{dadosController.ajustaDataVenda(venda._data)}</td>
+                                    <td>{venda._vendedor._nome}</td>
+                                    <td>{venda._produto._nome}</td>
+                                    <td>{venda._produto._id}</td>
+                                    <td>{venda._cliente._nome}</td>
+                                    <td>{venda._cliente.cpfcnpj}</td>
+                                    <td>{venda._cliente._segmento}</td>
+                                    <td>{dadosController.mascaraPreco(venda._valor.toString())}</td>
+                                    <td>{venda.formaPagamento}</td>
+                                    <button className={Style.btn}>Editar</button>
+                                    <button className={Style.btn} onClick={() => deletaVendas(venda.id)}>Apagar</button>
                                 </tr>
-                            </thead>
-                            <tbody>
-                                {/* <td>{this.state.vendas[0].usuario.nome}</td> */}
-                                {vendas.map((venda: any,index: any) => index%2==0? (
-                                    <tr key={index}>
-                                        <td>{venda.id}</td>
-                                        <td>{dadosController.ajustaDataVenda(venda._data)}</td>
-                                        <td>{venda._vendedor._nome}</td>
-                                        <td>{venda._produto._nome}</td>
-                                        <td>{venda._produto._id}</td>
-                                        <td>{venda._cliente._nome}</td>
-                                        <td>{venda._cliente.cpfcnpj}</td>
-                                        <td>{venda._cliente._segmento}</td>
-                                        <td>{dadosController.mascaraPreco(venda._valor.toString())}</td>
-                                        <td>{venda.formaPagamento}</td>
-                                        <button className={Style.btn}>Editar</button>
-                                        <button className={Style.btn} onClick={() => this.deletaVendas(venda.id)}>Apagar</button>
-                                    </tr>
-                                ):null)}
-                            </tbody>
-                        </table>
-                    </section>
-                </div>
-
-            </>
-        )
-    }
+                            ): null)}
+                        </tbody>
+                    </table>
+                </section>
+            </div>
+        </>
+    )
 }
